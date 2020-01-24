2f9647230c68221661d7602bfaf5847619ebec84
beaulibe <benoit.beaulieu@cegepsherbrooke.qc.ca>
2020-01-24 14:15:30 -0500

    Demo Sem3 Struct

---
+++ Demos/Sem3_Struct.X/main.c
@@ -0,0 +1,25 @@
+/*
+ * File:   main.c
+ * Author: tge
+ *
+ * Created on 24 janvier 2020, 14:10
+ */
+
+
+#include <xc.h>
+
+struct moteur 
+{
+    int vitesse;
+    int temperature;
+    int courant;
+};
+
+struct moteur stMotMoulin;
+
+void main(void) 
+{
+
+
+
+}
